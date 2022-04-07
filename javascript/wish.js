7e13e32ea75d617338d94f3be18ade6ef0da52de
Alfiya Siddique <86224794+AlfiyaSiddique@users.noreply.github.com>
2022-04-07 20:21:54 +0530

    Update wish.js

--- javascript/wish.js
+++ javascript/wish.js
@@ -1,3 +1,13 @@
+let position = 0;
+let colors = ["red","blue","green","yellow","grey","pink","orange","whitesmoke","bluevoilet"]
+let poppers = document.getElementsByTagName("i");
+for (let i = 0; i < poppers.length; i++) {
+ poppers[i].style.left = position;
+    position++;
+    console.log(poppers[i])
+}
+
+
 function birthdayWisher() {
 
     //  Variables
@@ -9,7 +19,7 @@ function birthdayWisher() {
     let currentYearBirthDate;
     let convertTime ;
 
-    // First step to find whether the person have borned or not
+    
     if (usseRegex.test(userName) && today.getFullYear()>birthDateObject.getFullYear() ||usseRegex.test(userName) &&
      today.getFullYear() == birthDateObject.getFullYear()  && today.getDate()>=birthDateObject.getDate() && today.getMonth()>= birthDateObject.getMonth()) {
 
@@ -30,9 +40,11 @@ function birthdayWisher() {
         let fullBirth = birthMonth + "-" + birthDate;
 
         if(fullBirth == fulldate){
+            let audio = new Audio("Piano.mp3");
+            audio.play();
+            confet();
             document.getElementById("output1").
-                innerHTML = `Oh! Today is ${today.getFullYear()}-
-                ${fulldate}<br> It's your birthday.`
+                innerHTML = `Oh! Today is ${today.getFullYear()}-${fulldate}<br> It's your birthday.`
     
                 document.getElementById("output2").
                 innerText = `Happy Birthday ${userName}`;
@@ -52,7 +64,7 @@ function birthdayWisher() {
                     }
                     else if (convertTime <= 10) { document.getElementById("output1").
                     innerText = `Only ${convertTime}
-                     days are left for your birthday!` }
+                     days is left for your birthday!` }
                     else {
                         document.getElementById("output1").
                         innerText = `${convertTime}
@@ -68,14 +80,14 @@ function birthdayWisher() {
                   console.log(differ)
                   convertTime = Math.ceil(differ/(3600*1000*24));
 
-                  if (convertTime == 1) { document.getElementById("output1").
-                        innerText = `Only ${convertTime}
-                         day is left for your birthday!` }
-                        else if (convertTime <= 10) {
+                  if (convertTime <= 10) {
                             document.getElementById("output1").
                             innerText = `Only ${convertTime}
                              days are left for your birthday!`
                         }
+                        else if (convertTime == 1) { document.getElementById("output1").
+                        innerText = `Only ${convertTime}
+                         day is left for your birthday!` }
                         else {
                             document.getElementById("output1").
                             innerText = `${convertTime}
@@ -91,3 +103,61 @@ function birthdayWisher() {
     }
 
 }
+
+birthdayWisher();
+
+
+function randomNum(num){
+    let random = Math.floor(Math.random()*num);
+    return random;
+}
+
+function confet(){
+    let ribbons = ["blue","green","yellow","red","purple","cyan","burlywood","pink","orange","darkmagenta"]
+    let topos = 0;
+    let xpos = 0;
+    
+    for(let i = 0; i<300;i++){
+        let divs = document.createElement("div");
+        let parent = $("body");
+        parent[0].appendChild(divs);
+        let random = randomNum(10);
+        divs.style = `background-color:${ribbons[random]}`;
+        divs.style.top = `${topos}px`;
+
+        if(i<100){
+            divs.setAttribute("class","con")
+           divs.style.left = `${xpos}px`
+        }else if(i<200 && i>100){
+            divs.setAttribute("class","con3");
+           divs.style.left = `${xpos}px`
+        }else{
+        divs.setAttribute("class","con2")
+        divs.style.right = `${xpos}px`
+        
+        }
+        topos = randomNum(665);
+        xpos = randomNum(1600);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
